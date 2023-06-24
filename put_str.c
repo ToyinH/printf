12fa1d1721bd2e22bdf931d58f90548e309c31f5
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-06-24 11:37:54 +0000

    print string function

---
+++ put_str.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * put_str - function to print a string
+ * @str: the string to be printed
+ *
+ * Return: return the number of character printed
+ */
+
+int put_str(char *str)
+{
+	int r_val = 0;
+	int i = 0;
+
+	if (str)
+	{
+		while (str[i] != '\0')
+		{
+			put_char(str[i]);
+			r_val++;
+			i++;
+		}
+	}
+	return (r_val);
+}
