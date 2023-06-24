73708928f168fe3b178f22f68094942dee3ea58c
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-06-24 14:52:42 +0000

    updated printf file. (debugged)

---
+++ printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _printf - my printf function
+ * @format: string and specifier
+ *
+ * Return: return the number of character printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int print_counts = 0;
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i] != '\0')
+		{
+			if (format[i] == '%')
+			{
+				i++;
+				print_counts = specifier_func(format[i], list);
+			}
+			else
+			{
+				put_char(format[i]);
+				print_counts++;
+			}
+			i++;
+		}
+		va_end(list);
+	}
+	return (print_counts);
+}
