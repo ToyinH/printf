5a6c941e916e109bb29acfc108bd3673a59819f3
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-06-27 14:28:19 +0000

    modified to handle buffer

--- put_str.c
+++ put_str.c
@@ -9,15 +9,25 @@
 
 int put_str(char *str)
 {
+	static char buffer[BUFFER_SIZE];
+	static int buffer_index = 0;
 	int r_val = 0;
+	int r_val1 = 0;
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		put_char(str[i]);
-		r_val++;
+		r_val1 = 0;
+		buffer[buffer_index++] = str[i];
+		r_val = (r_val + 1) + r_val1;
 		i++;
+		if (buffer_index == BUFFER_SIZE)
+		{
+			r_val1 = write(1, buffer, buffer_index);
+			buffer_index = 0;
+		}
 	}
+	write(1, buffer, buffer_index);
 	return (r_val);
 }
 
