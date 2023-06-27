d1c6e33d29201a3bab35f4e23d86f01745574c58
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-06-27 14:36:16 +0000

    considering buffer size

--- put_char.c
+++ put_char.c
@@ -8,5 +8,22 @@
  */
 int put_char(char a)
 {
-	return (write(1, &a, 1));
+	static char buffer[BUFFER_SIZE];
+	static int buffer_index = 0;
+	int r_val = 0;
+	int r_val1 = 0;
+
+	if (buffer_index < BUFFER_SIZE)
+	{
+		return (write(1, &a, 1));
+	}
+	else if (buffer_index >= BUFFER_SIZE)
+	{
+		r_val1 = write(1, buffer, buffer_index);
+		buffer_index = 0;
+		buffer[buffer_index++] = a;
+		r_val = (r_val + 1) + r_val1;
+		write(1, &a, buffer_index);
+	}
+	return (r_val);
 }
